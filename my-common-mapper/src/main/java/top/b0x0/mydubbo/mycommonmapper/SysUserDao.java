e66afaaaf130edc8ca49c9a0e0ade159dd6dd4a8
tlh <im.linghan@qq.com>
2021-06-01 21:59:26 +0800

    dubbo project update

---
+++ my-common-mapper/src/main/java/top/b0x0/mydubbo/mycommonmapper/SysUserDao.java
@@ -0,0 +1,16 @@
+package top.b0x0.mydubbo.mycommonmapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.b0x0.mydubbo.mycommon.pojo.system.SysUser;
+
+/**
+ * 系统用户(SysUser)表数据库访问层
+ *
+ * @author TANG
+ * @date 2020-12-17
+ * @since 1.0.0
+ */
+public interface SysUserDao extends BaseMapper<SysUser> {
+
+
+}
